ac93ecf82706a9cb0de144415462abe9ecbbc1ca
Tahseen Hossain <tahseenhossain11@gmail.com>
2025-03-08 15:21:03 +0600

    Updated project

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ import Rooms from "./Rooms";
 
 const Home = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
             <Banner></Banner>
             <div className="mx-2 md:mx-8 lg:mx-24">
                 <h3 className="text-5xl md:text-7xl lg:text-7xl text-center text-[#E0B973] font-bold my-12">Our Special Winter Offers</h3>
